3d77610ff15b1e79f72718c087e1c31fd038950b
nivtal9 <nivtal9@gmail.com>
2020-05-24 20:34:42 +0300

    inital commit7

--- Demo.cpp
+++ Demo.cpp
@@ -1,17 +1,16 @@
 /**
   Demo for the war-game exercise.
-
   @author Oz Levi
   @since  2020-05
 **/
 
 #include <iostream>
-#include "DemoGame.hpp"  
+#include "DemoGame.hpp"
 
 using namespace std;
 
 int main() {
-	WarGame::DemoGame demoGame;
-	cout << "The winner is:\n " << demoGame.play() << endl;
+    WarGame::DemoGame demoGame;
+    cout << "The winner is:\n " << demoGame.play() << endl;
     return 0;
 }
