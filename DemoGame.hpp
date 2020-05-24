0753057307e7b5e1c05de8f1a73972832d578ead
nivtal9 <nivtal9@gmail.com>
2020-05-24 19:12:09 +0300

    inital commit2

--- DemoGame.hpp
+++ DemoGame.hpp
@@ -11,19 +11,19 @@
 #include "Board.hpp"
 
 namespace WarGame {
-	class DemoGame {
-	 private:
-		Board board;
-	 public:
+    class DemoGame {
+    private:
+        Board board;
+    public:
 
- 		/**
-		 * The constructor initilizes the board and places the soldiers.
-		 */
-		DemoGame();
-		
-		/**
-		 * This function runs a sample play of the game and returns the winner (1 or 2, or 0 in case of tie).
-		 */
-		uint play(); 
-	};
+        /**
+        * The constructor initilizes the board and places the soldiers.
+        */
+        DemoGame();
+
+        /**
+         * This function runs a sample play of the game and returns the winner (1 or 2, or 0 in case of tie).
+         */
+        uint play();
+    };
 }
